e18aa395b40faa143ed82d5301f1964be5a8f12c
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-06-08 18:20:53 +0200

    [dev] (feat): query toolkit by id

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,3 +1,5 @@
+import { UserInputError } from 'apollo-server-micro'
+
 const knex = require('knex')({
   client: 'pg',
   version: '10.6',
@@ -10,9 +12,24 @@ const knex = require('knex')({
   searchPath: 'sc_tool'
 })
 
+const isValidUUID = uuid =>
+  /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(
+    uuid
+  )
+
 export default {
   Query: {
     hello: () => 'toolkit service says hello',
-    toolkits: async () => await knex('toolkit').select()
+    toolkits: async () => await knex('toolkit').select(),
+    toolkit: async (root, { id }) => {
+      if (!isValidUUID(id)) throw new UserInputError('INVALID_UUID')
+      const kit = (await knex('toolkit').where({ id }))[0]
+      if (!kit) throw new UserInputError('DOES_NOT_EXIST')
+      return kit
+    }
+  },
+
+  Toolkit: {
+    canvas: ({ canvas }) => JSON.stringify(canvas)
   }
 }
